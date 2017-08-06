b45cf601645b48f0225721edfcdc57020fc3a551
Daniel Klein <danielklein@airpost.net>
2017-08-06 18:40:16 +1000

    Rearrange tests Add tests for isset() Add note to README.md about not being able to disable unset()

--- README.md
+++ README.md
@@ -9,15 +9,18 @@ require_once 'class_protection.class.php';
 
 class MyClass {
   use ClassProtection;
-  
+
   // Remainder of class code
 }
 ```
 
-It is now no longer possible to dynamically create new `public` properties 
+It is now no longer possible to dynamically create new `public` properties
 at run-time from either inside or outside the class `MyClass`,
 but any existing `public` properties can still be accessed and changed.
 All `public` properties must now be declared before they're used,
 just like `protected` and `private` properties.
 
 That's it!
+
+One caveat: `unset()` can still be called on public properties and delete them from the object.
+It's best to completely avoid using public properties for a robust class with locked-down behaviour.
--- tests/unit/ClassProtectionTest.php
+++ tests/unit/ClassProtectionTest.php
@@ -14,28 +14,16 @@ class ClassProtectionTest extends \Codeception\Test\Unit {
   }
 
   // tests
+  // __get()
   public function testShouldAccessAnExistingPublicProperty() {
     $protected_class = new ProtectedClass;
     $this->assertSame(1, $protected_class->foo);
-  }
-
-  public function testShouldSetAnExistingPublicProperty() {
-    $protected_class = new ProtectedClass;
-    $protected_class->foo = 42;
-    $this->assertSame(42, $protected_class->foo);
   }
 
   public function testShouldGenerateAFatalErrorWhenAccessingAnExistingProtectedProperty() {
     $protected_class = new ProtectedClass;
     $this->tester->expectException('Exception', function () use ($protected_class) {
       $dummy = $protected_class->bar;
-    });
-  }
-
-  public function testShouldGenerateAFatalErrorWhenSettingAnExistingProtectedProperty() {
-    $protected_class = new ProtectedClass;
-    $this->tester->expectException('Exception', function () use ($protected_class) {
-      $protected_class->bar = 42;
     });
   }
 
@@ -46,17 +34,31 @@ class ClassProtectionTest extends \Codeception\Test\Unit {
     });
   }
 
-  public function testShouldGenerateAFatalErrorWhenSettingAnExistingPrivateProperty() {
-    $protected_class = new ProtectedClass;
-    $this->tester->expectException('Exception', function () use ($protected_class) {
-      $protected_class->baz = 42;
-    });
-  }
-
   public function testShouldGenerateAFatalErrorWhenAccessingANonExistentPublicProperty() {
     $protected_class = new ProtectedClass;
     $this->tester->expectException('Exception', function () use ($protected_class) {
       $dummy = $protected_class->qux;
+    });
+  }
+
+  // __set()
+  public function testShouldSetAnExistingPublicProperty() {
+    $protected_class = new ProtectedClass;
+    $protected_class->foo = 42;
+    $this->assertSame(42, $protected_class->foo);
+  }
+
+  public function testShouldGenerateAFatalErrorWhenSettingAnExistingProtectedProperty() {
+    $protected_class = new ProtectedClass;
+    $this->tester->expectException('Exception', function () use ($protected_class) {
+      $protected_class->bar = 42;
+    });
+  }
+
+  public function testShouldGenerateAFatalErrorWhenSettingAnExistingPrivateProperty() {
+    $protected_class = new ProtectedClass;
+    $this->tester->expectException('Exception', function () use ($protected_class) {
+      $protected_class->baz = 42;
     });
   }
 
@@ -66,4 +68,56 @@ class ClassProtectionTest extends \Codeception\Test\Unit {
       $protected_class->qux = 42;
     });
   }
+
+  // __isset()
+  public function testShouldReturnTrueForAnExistingSetPublicProperty() {
+    $protected_class = new ProtectedClass;
+    $this->assertTrue(isset($protected_class->foo));
+  }
+
+  public function testShouldReturnFalseForAnExistingSetProtectedProperty() {
+    $protected_class = new ProtectedClass;
+    $this->assertFalse(isset($protected_class->bar));
+  }
+
+  public function testShouldReturnFalseForAnExistingSetPrivateProperty() {
+    $protected_class = new ProtectedClass;
+    $this->assertFalse(isset($protected_class->baz));
+  }
+
+  public function testShouldReturnFalseForANonExistingSetPublicProperty() {
+    $protected_class = new ProtectedClass;
+    $this->assertFalse(isset($protected_class->qux));
+  }
+
+  //// __unset()
+  //// Unfortunately, PHP only seems to call __unset() when the named public property doesn't exist,
+  //// making it useless for this purpose
+  //public function testShouldGenerateAFatalErrorWhenUnsettingAnExistingPublicProperty() {
+  //  $protected_class = new ProtectedClass;
+  //  $this->tester->expectException('Exception', function () use ($protected_class) {
+  //    unset($protected_class->foo);
+  //  });
+  //}
+  //
+  //public function testShouldGenerateAFatalErrorWhenUnsettingAnExistingProtectedProperty() {
+  //  $protected_class = new ProtectedClass;
+  //  $this->tester->expectException('Exception', function () use ($protected_class) {
+  //    unset($protected_class->bar);
+  //  });
+  //}
+  //
+  //public function testShouldGenerateAFatalErrorWhenUnsettingAnExistingPrivateProperty() {
+  //  $protected_class = new ProtectedClass;
+  //  $this->tester->expectException('Exception', function () use ($protected_class) {
+  //    unset($protected_class->baz);
+  //  });
+  //}
+  //
+  //public function testShouldGenerateAFatalErrorWhenUnsettingANonExistingPublicProperty() {
+  //  $protected_class = new ProtectedClass;
+  //  $this->tester->expectException('Exception', function () use ($protected_class) {
+  //    unset($protected_class->qux);
+  //  });
+  //}
 }
